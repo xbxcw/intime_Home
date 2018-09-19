073f171adc478d2a2ffe60e01e1b4670e38e4057
啦啦啦 <hanyuncheng@outlook.com>
2018-09-19 12:45:35 +0800

    完善Gallery App
    设置media路径和网站

--- gallery/models.py
+++ gallery/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
-
+    description = models.CharField(default='简介', max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='标题', max_length=50)
+    
     def __str__(self):
-        return '标题'
+        return self.title
--- hexgon/settings.py
+++ hexgon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 我的APP
     'intime.apps.IntimeConfig',
     'gallery.apps.GalleryConfig',
 ]
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
